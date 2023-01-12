1e4780192d59273f7db01ade79955042b45a6a2c
Fahd Mohammed <fahdmoh.1@gmail.com>
2023-01-12 23:09:46 +0000

    learnt how to manipulate static files in django

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-	context = {
-		'name': 'Fahd Agodzo',
-		'age': 25,
-		'nationality': 'Ghanaian'
-	}
-	return render(request,'index.html',context)
+	return render(request,'index.html')
+
+
+def counter(request):
+	text = request.POST['text']
+	amount_of_words = len(text.split())
+	return render(request,'counter.html', {'amount':amount_of_words})
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'myproject.urls'
@@ -118,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS= (os.path.join(BASE_DIR,'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
